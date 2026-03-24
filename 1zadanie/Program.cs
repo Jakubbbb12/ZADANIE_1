b7b736dd4828297e9c30e0f713f3baab9f7c0a1c
Jakubbbb12 <jakub.majcher@student.wsb.edu.pl>
2026-03-24 20:23:43 +0100

    Zadanie 1
    Pierwszy prototyp. Nie wykryto żadnych błędów.

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a, b, wynik;
+        Console.WriteLine("Prosty kalkulator dwóch liczb");
+        Console.WriteLine("Podaj liczbę A:");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Podaj liczbę B:");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Wybierz działanie: +, -, *, /");
+        string operacja = Console.ReadLine();
+        switch (operacja)
+        {
+            case "+":
+                wynik = a + b;
+                Console.WriteLine("Wynik: " + wynik);
+                break;
+            case "-":
+                wynik = a - b;
+                Console.WriteLine("Wynik: " + wynik);
+                break;
+            case "*":
+                wynik = a * b;
+                Console.WriteLine("Wynik: " + wynik);
+                break;
+            case "/":
+                if (b != 0)
+                {
+                    wynik = a / b;
+                    Console.WriteLine("Wynik: " + wynik);
+                }
+                else
+                {
+                    Console.WriteLine("Nie można dzielić przez zero!");
+                }
+                break;
+            default:
+                Console.WriteLine("Nieznana operacja!");
+                break;
+        }
+    }
+}
